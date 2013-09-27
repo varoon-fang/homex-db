b2ca373cc00dc5e00491c42a2ee9a47a4cd6929e
varoon-fang <FaNg@varoons-MacBook-Pro.local>
2013-09-27 22:27:43 +0700

    update solution menu left

---
+++ application/views/front/temp/solution-list.php
@@ -0,0 +1,42 @@
+<div class="col-sm-12 col-md-3 col-lg-3 col-xs-12 ">
+
+     <aside id="solution-list" >
+
+               <div class="list-group">
+
+     	         <a href="<?= site_url("solution/bathroom");?>" class="list-group-item active ">
+     	           <h4 class="list-group-item-heading">Bathroom Solution <span class="text-color-white glyphicon glyphicon-chevron-right pull-right"></span></h4>
+     	           <p class="list-group-item-text">โซลูชั่นห้องน้ำ</p>
+     	         </a>
+
+     	         <a href="#" class="list-group-item ">
+     	           <h4 class="list-group-item-heading">Landscape Solution <span class="text-color-white glyphicon glyphicon-chevron-right pull-right"></span></h4>
+     	           <p class="list-group-item-text">โซลูชั่นภูมิทัศน์ภายนอก</p>
+     	         </a>
+
+     	         <a href="#" class="list-group-item ">
+     	           <h4 class="list-group-item-heading">Surface Covering Solution <span class="text-color-white glyphicon glyphicon-chevron-right pull-right"></span></h4>
+     	           <p class="list-group-item-text">โซลูชั่นห้องน้ำ</p>
+     	         </a>
+
+     	         <a href="#" class="list-group-item ">
+     	           <h4 class="list-group-item-heading">Ceiling &amp; Wall Solution <span class="text-color-white glyphicon glyphicon-chevron-right pull-right"></span></h4>
+     	           <p class="list-group-item-text">โชลูชั่นฝ้าและผนัง</p>
+     	         </a>
+
+     	         <a href="#" class="list-group-item ">
+     	           <h4 class="list-group-item-heading">Roof Solution <span class="text-color-white glyphicon glyphicon-chevron-right pull-right"></span></h4>
+     	           <p class="list-group-item-text">โซลูชั่นหลังคา </p>
+     	         </a>
+
+
+     	         <a href="#" class="list-group-item ">
+     	           <h4 class="list-group-item-heading">Kitchen Solution <span class="text-color-white glyphicon glyphicon-chevron-right pull-right"></span></h4>
+     	           <p class="list-group-item-text">โซลูชั่นห้องครัว</p>
+     	         </a>
+
+ 	       </div><!--end list group-->
+
+           </aside>
+
+ </div><!-- /.col-sm-12 -->
